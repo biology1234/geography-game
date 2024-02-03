22df956b873a2722fa70be58b96c7ca5b2f92463
medicine-for-students <biology.for.propose.students@gmail.com>
2024-02-03 22:20:33 +0200

    Update myProfileScript.js

--- myProfileScript.js
+++ myProfileScript.js
@@ -43,38 +43,33 @@ signOutBttn.addEventListener('click', function signOut12() {
   })
 });*/
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-      import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-      import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-      // TODO: Add SDKs for Firebase products that you want to use
-      // https://firebase.google.com/docs/web/setup#available-libraries
-    
-      // Your web app's Firebase configuration
-      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-      const firebaseConfig = {
-        apiKey: "AIzaSyB_TgoZVBgp8xqflkvVJbRbPfvTWnyYfOc",
-        authDomain: "biomedicine-29379.firebaseapp.com",
-        databaseURL: "https://biomedicine-29379-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "biomedicine-29379",
-        storageBucket: "biomedicine-29379.appspot.com",
-        messagingSenderId: "1023601874434",
-        appId: "1:1023601874434:web:da6c166d9d0598a237f91b",
-        measurementId: "G-58F1VKGWFK"
-      };
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-    let logInBttn = document.getElementById('logInBttn');
-    let myProfileBttn = document.getElementById('myProfileBttn');
+const firebaseConfig = {
+    apiKey: "AIzaSyB_TgoZVBgp8xqflkvVJbRbPfvTWnyYfOc",
+    authDomain: "biomedicine-29379.firebaseapp.com",
+    databaseURL: "https://biomedicine-29379-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "biomedicine-29379",
+    storageBucket: "biomedicine-29379.appspot.com",
+    messagingSenderId: "1023601874434",
+    appId: "1:1023601874434:web:da6c166d9d0598a237f91b",
+    measurementId: "G-58F1VKGWFK"
+};
 
-      // Initialize Firebase
-      const app = initializeApp(firebaseConfig);
-      const analytics = getAnalytics(app);
-      const auth = getAuth();
-     onAuthStateChanged(function(user) {
-        if (user) {
-          // Потребителят е логнат
-         alert('има');
-        } else {
-          // Няма логнат потребител
-          alert('няма');
-        }
-      });
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
 
+// Функция за проверка на състоянието на аутентикацията
+function checkAuthState() {
+  onAuthStateChanged(auth, function(user) {
+    if (user) {
+      // Потребителят е логнат
+      alert('Има логнат потребител');
+    } else {
+      // Няма логнат потребител
+      alert('Няма логнат потребител');
+    }
+  });
+}
