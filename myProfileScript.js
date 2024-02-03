49b201a3bef99bd0b8ac4d597122534f3e437e49
medicine-for-students <biology.for.propose.students@gmail.com>
2024-02-03 22:12:44 +0200

    Update myProfileScript.js

--- myProfileScript.js
+++ myProfileScript.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+/*import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
@@ -41,4 +41,42 @@ signOutBttn.addEventListener('click', function signOut12() {
       });
     }
   })
+});*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+      import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+      import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
+    
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      const firebaseConfig = {
+        apiKey: "AIzaSyB_TgoZVBgp8xqflkvVJbRbPfvTWnyYfOc",
+        authDomain: "biomedicine-29379.firebaseapp.com",
+        databaseURL: "https://biomedicine-29379-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "biomedicine-29379",
+        storageBucket: "biomedicine-29379.appspot.com",
+        messagingSenderId: "1023601874434",
+        appId: "1:1023601874434:web:da6c166d9d0598a237f91b",
+        measurementId: "G-58F1VKGWFK"
+      };
+
+    let logInBttn = document.getElementById('logInBttn');
+    let myProfileBttn = document.getElementById('myProfileBttn');
+
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
+      const auth = getAuth();
+     onAuthStateChanged(auth, (user) => {
+    if (user) {
+  alert('има user');
+    const uid = user.uid;
+    // ...
+
+  } else {
+    // User is signed out
+    // ...
+    alert('error');
+  }
 });
