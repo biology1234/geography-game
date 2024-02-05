d4974a0f0e962ac52f601705295e8b936ea66a1a
medicine-for-students <biology.for.propose.students@gmail.com>
2024-02-05 10:30:42 +0200

    ег

--- myProfileScript.js
+++ myProfileScript.js
@@ -1,3 +1,80 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+        import { getAuth, signOut  } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+          apiKey: "AIzaSyB_TgoZVBgp8xqflkvVJbRbPfvTWnyYfOc",
+          authDomain: "biomedicine-29379.firebaseapp.com",
+          databaseURL: "https://biomedicine-29379-default-rtdb.europe-west1.firebasedatabase.app",
+          projectId: "biomedicine-29379",
+          storageBucket: "biomedicine-29379.appspot.com",
+          messagingSenderId: "1023601874434",
+          appId: "1:1023601874434:web:da6c166d9d0598a237f91b",
+          measurementId: "G-58F1VKGWFK"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+        const auth = getAuth();
+        let signOutBttn = document.getElementById('signOut');
+        signOutBttn.addEventListener('click', function signOut12(){
+       signOut(auth).then(() => {
+   // Sign-out successful.
+   alert('Излизането от профила бе успешно!');
+   window.location.href = "index.html";
+          }).catch((error) => {
+      // An error happened.
+      alert('Грешка, моля опитайте по-късно.');
+         });
+        });
+        
+        
+        const database = firebase.database();
+        const reference = database.ref('/път/до/конкретен/възел');
+        
+        // Извлечете данни веднъж (без следене за промени)
+        reference.once('value')
+          .then((snapshot) => {
+            const data = snapshot.val();
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error("Грешка при извличане на данни:", error);
+          });
 
 
-    
+        function generateAvatar() {
+            // Вземете първите букви от името и фамилията
+            let firstName = 
+            const initials = (firstName[0] || '') + (lastName[0] || '');
+      
+            // Създайте canvas елемент
+            const canvas = document.getElementById('avatarCanvas');
+            const ctx = canvas.getContext('2d');
+      
+            // Нарисувайте кръг със случайен цвят
+            ctx.beginPath();
+            ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+            ctx.fillStyle = getRandomColor();
+            ctx.fill();
+      
+            // Напишете първите букви в средата на кръга
+            ctx.font = '30px Arial';
+            ctx.fillStyle = 'white';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillText(initials, 100, 100);
+          }
+      
+          function getRandomColor() {
+            // Генерирайте случаен цвят в шестнадесетичен формат
+            return '#' + Math.floor(Math.random()*16777215).toString(16);
+          }
+      
+          // Пример за използване
+          generateAvatar();
