cbef370e781e0da24365214e7b689ba603fbf349
medicine-for-students <biology.for.propose.students@gmail.com>
2024-02-23 18:43:42 +0200

    fjyh

--- beforeStart.js
+++ beforeStart.js
@@ -1,21 +1,6 @@
-const div = document.getElementById('bluredLogIn');
-let bluredBackground = document.getElementById('container');
-const passwordShowingImg = document.getElementById('passwordShowingImg2'); 
-const password = document.getElementById('password2'); 
-
-passwordShowingImg.addEventListener('click', function(event){
-    event.preventDefault();
-    if (password.type === 'password'){
-        password.type = 'text';
-    }
-    else {
-        password.type = 'password';
-    }
-});
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
- import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+ import { onAuthStateChanged, signInWithEmailAndPassword, GithubAuthProvider, getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithRedirect, getRedirectResult, FacebookAuthProvider  } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -32,27 +17,101 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
    measurementId: "G-58F1VKGWFK"
  };
 
-let logInBttn = document.getElementById('logInBttn');
-let myProfileBttn = document.getElementById('myProfileBttn');
+const bluredLogIn = document.getElementById('bluredLogIn');
+const container = document.getElementById('container');
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+ const analytics = getAnalytics(app); // Use the app object here
  const auth = getAuth();
  
  onAuthStateChanged(auth, (user) => {
-     if (user) {
-         // User is signed in
-         bluredBackground.style.filter = 'none';
-         div.style.display = 'none';
-         alert('OK');
-         const uid = user.uid;
-         
-         // Additional logic for signed-in user
-     } 
-     else{
-        alert('error');
-     }
+   if (user) {
+     
+     const uid = user.uid;
+     bluredLogIn.style.display = 'none';
+     container.style.filter = 'none';
+ 
+   } else {
+     bluredLogIn.style.display ='block';
+     container.style.filter ='blur(4px)';
+   }
  });
 
- 
+
+
+ document.getElementById("submit2").addEventListener('click', function (event) {
+    event.preventDefault();
+    const email = document.getElementById('email2').value;
+    const password = document.getElementById('password2').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+// Signed in 
+const user = userCredential.user;
+
+// ...
+})
+.catch((error) => {
+const errorCode = error.code;
+const errorMessage = error.message;
+alert('Моля, въведете валиден имейл адрес или парола');
+});
+});
+
+document.getElementById("submit2").addEventListener('click', function (event) {
+
+    set(ref(db, 'Потребители/' + document.getElementById("name").value), {
+        name: document.getElementById("name").value,
+        lastName: document.getElementById("lastName").value,
+    });
+});
+
+const google = document.getElementById('google2');
+google.addEventListener('click', (event) => {
+    event.preventDefault();
+    signInWithRedirect(auth, new GoogleAuthProvider());
+  
+});
+
+const provider = new FacebookAuthProvider();
+const facebook = document.getElementById('facebook2');
+facebook.addEventListener('click', (event) => {
+  event.preventDefault();
+  signInWithRedirect(auth, new FacebookAuthProvider());
+  
+});
+const provider3 = new GithubAuthProvider();
+const github = document.getElementById('github2');
+github.addEventListener('click', (event) =>{
+  event.preventDefault();
+  signInWithRedirect(auth, new GithubAuthProvider() );
+
+});
+onAuthStateChanged(auth, (user) => {
+if (user) {
+
+
+const uid = user.uid;
+// ...
+
+} else {
+// User is signed out
+// ...
+
+}
+});
+
+
+
+const passwordShowingImg2 = document.getElementById('passwordShowingImg2');
+let password2 = document.getElementById('password2');
+passwordShowingImg2.addEventListener('click', function passwordShowing(event){
+event.preventDefault();
+if (password2.type === 'password'){
+    password2.type = 'text';
+}
+else {
+    password2.type = 'password';
+}
+});
