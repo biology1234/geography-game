d6eb6928c097907657e42da735efc7e8a9b9d397
medicine-for-students <biology.for.propose.students@gmail.com>
2024-03-01 09:35:33 +0200

    gjkhjk

---
+++ addExam.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById('addQuestion').addEventListener('click', function test() {
+
+
+
+
+
+    /*const question = document.createElement('input');
+    question.type = 'text'; 
+question.placeholder = 'Първи въпрос'; 
+question.style.position = 'relative';
+question.style.top = '45px';
+question.style.right = '200px'
+    document.body.appendChild(question);*/
+});
+
